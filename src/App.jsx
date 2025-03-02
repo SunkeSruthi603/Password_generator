456203209529b5c4ed3966670e9fa6335249126c
SunkeSruthi603 <sunkesruthi323@gmail.com>
2025-03-02 20:14:30 +0530

    Adding functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,59 @@
 import React from "react";
 
 function App() {
+  let [password, setPassword] = React.useState("")
+  let [passwordLength, setPasswordLength] = React.useState(8)
+
+
+  const handleLengthIncrese = (e) => {
+    e.preventDefault()
+
+    setPasswordLength(passwordLength + 1)
+  }
+
+  const handleLengthDecrese = (e) => {
+    e.preventDefault()
+
+    if(passwordLength === 8) return;
+
+    setPasswordLength(passwordLength - 1)
+  }
+
+  const handlePasswordGenerate = React.useCallback(() => {
+    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let pass =""
+
+    for(let i = 0; i < passwordLength; i++){
+      pass = pass + chars[Math.floor(Math.random() * chars.length)]
+    }
+
+    setPassword(pass)
+  }, [passwordLength])
+
+  React.useEffect(() => {
+    handlePasswordGenerate()
+
+  }, [passwordLength])
+
   return (
-    <div className="h-screen w-screen flex items-center justify-center flex-col">
-      <input type="text" className="h-10 w-96 outline-none border border-slate-800 rounded-lg px-5"></input>
-      <div>
-        <button className="h-10 px-5 text-xl font-bold bg-slate-700 text-slate-200 rounded-lg outline-none">+</button>
-        <input type="text"/>
-        <button className="h-10 px-5 text-xl font-bold bg-slate-700 text-slate-200 rounded-lg outline-none">-</button>
+    <div className="h-screen w-screen flex items-center justify-center flex-col gap-y-5">
+      <h1 className="text-3xl font-bold text-slate-800">Password Generator</h1>
+      <input type="text" className="h-10 w-96 outline-none border border-slate-800 
+      rounded-lg px-5 text-center"value={password} readOnly></input>
+      
+      <div className="flex items-center justify-center gap-x-2">
+        
+        <button className="h-10 px-5 text-xl font-bold bg-slate-700 
+        text-slate-200 rounded-lg outline-none" onClick={handleLengthIncrese}>+</button>
+        
+        <input type="text" className="h-10 px-5 text-center outline-none
+        border border-slate-800 rounded-lg" value={passwordLength} readOnly/>
+        
+        <button className="h-10 px-5 text-xl font-bold bg-slate-700 
+        text-slate-200 rounded-lg outline-none" onClick={handleLengthDecrese}>-</button>
+
       </div>
+
     </div>
   );
 }
